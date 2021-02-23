b3a5a84a5a4102f91e84b80daef8bc7916ee2df1
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-02-23 20:10:59 +0900

    audioの実装・rangeを修正

--- js/index.js
+++ js/index.js
@@ -1,13 +1,95 @@
-  window.onload = function() {
 
+const audio1 = document.querySelector("#audio1"); 
+const audio2 = document.querySelector("#audio2"); 
+const audio3 = document.querySelector("#audio3"); 
+const audio4 = document.querySelector("#audio4"); 
+const audio5 = document.querySelector("#audio5"); 
+
+const bgm1 = function() {
+  
+  if( ! audio1.paused ){
+    audio1.pause();
+  } else {
+    audio1.play();
+    audio1.volume = 0.5;
   }
-  function bgm1(){
+}
+const bgm2 = function() {
+
+  if( ! audio2.paused ){
+    audio2.pause();
+  } else {
+    audio2.play();
+    audio2.volume = 0.5;
   }
-  function bgm2(){
+}
+const bgm3 = function() {
+
+  if( ! audio3.paused ){
+    audio3.pause();
+  } else {
+    audio3.play();
+    audio3.volume = 0.5;
   }
-  function bgm3(){
+}
+const bgm4 = function() {
+
+  if( ! audio4.paused ){
+    audio4.pause();
+  } else {
+    audio4.play();
+    audio4.volume = 0.5;
   }
-  function bgm4(){
+}
+const bgm5 = function() {
+  if( ! audio5.paused ){
+    audio5.pause();
+  } else {
+    audio5.play();
+    audio5.volume = 1.0;
   }
-  function bgm5(){
+}
+const bgmall = function() {
+  if( audio1.paused && audio2.paused && audio3.paused && audio4.paused && audio5.paused  ) {
+    audio1.play();
+    audio2.play();
+    audio3.play();
+    audio4.play();
+    audio5.play();
+  } else {
+    audio1.pause();
+    audio2.pause();
+    audio3.pause();
+    audio4.pause();
+    audio5.pause();
   }
+}
+
+window.addEventListener('DOMContentLoaded', function(){
+
+  audio1.addEventListener('loadeddata', (e)=> {
+    audio1.muted = false;
+    audio1.autoplay = true;
+  });
+
+  audio2.addEventListener('loadeddata', (e)=> {
+    audio2.muted = false;
+    audio2.autoplay = true;
+  });
+
+  audio3.addEventListener('loadeddata', (e)=> {
+    audio3.muted = false;
+    audio3.autoplay = true;
+  });
+
+  audio4.addEventListener('loadeddata', (e)=> {
+    audio4.muted = false;
+    audio4.autoplay = true;
+  });
+
+  audio5.addEventListener('loadeddata', (e)=> {
+    audio5.muted = false;
+    audio5.autoplay = true;
+  });
+
+});
